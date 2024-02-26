228ed795df3b8433fd883f7c9cf62e8b1cfc9ccf
nataliebsmith <90297926+nataliebsmith@users.noreply.github.com>
2024-02-26 14:28:27 -0600

    second commit

--- page2.js
+++ page2.js
@@ -1,15 +1,4 @@
-// document2.addEventListener("DOMContentLoaded", () => { 
-    // var timeline_options = {
-    //     scrollTrigger: {
-    //       trigger: ".container",
-    //       // pin: true, // uncomment this value to pin the "container" in the same spot while animating
-    //       start: "top top", // this controls starting the animation when the top of container is at the top of the screen
-    //       end: "bottom bottom", // this controls ending the animation when the bottom of container is at the bottom of the screen
-    //       scrub: 0, // this controls animation timing relative to scroll
-    //       // scrub: 1 // uncomment this value to delay animations
-    //     },
-    //   }
-    // var timeline = gsap.timeline(timeline_options);
+
       gsap.to(".circcontent", {
         yPercent: -100,
         ease: "none",
@@ -31,4 +20,3 @@
           scrub: true
         }, 
       });
-// })
